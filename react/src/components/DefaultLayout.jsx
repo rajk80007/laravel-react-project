3cf9b91e199ee0bdc30ddc448c1ae535f4ee4776
raj <rajk80007@gmail.com>
2024-02-16 21:28:32 +0530

    Set Default Layout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -24,6 +24,11 @@ function classNames(...classes) {
 }
 
 export default function DefaultLayout() {
+
+  const logout = (ev) =>{
+    ev.preventDefault();
+    console.log("Logout");
+  }
   return (
     <>
       {/*
@@ -97,21 +102,20 @@ export default function DefaultLayout() {
                           leaveTo="transform opacity-0 scale-95"
                         >
                           <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
+                            <Menu.Item >
+                                
                                   <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? 'bg-gray-100' : '',
+                                    href="#"
+                                    onClick={(ev)=> logout(ev)}
+                                    className={                                    
                                       'block px-4 py-2 text-sm text-gray-700'
-                                    )}
+                                    }
                                   >
-                                    {item.name}
+                                    Signout
                                   </a>
-                                )}
+                                
                               </Menu.Item>
-                            ))}
+                            
                           </Menu.Items>
                         </Transition>
                       </Menu>
@@ -167,16 +171,15 @@ export default function DefaultLayout() {
                     </button>
                   </div>
                   <div className="mt-3 space-y-1 px-2">
-                    {userNavigation.map((item) => (
                       <Disclosure.Button
-                        key={item.name}
+                        
                         as="a"
-                        href={item.href}
+                        href="#"
+                        onClick={ (ev)=> logout(ev)}
                         className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
                       >
-                        {item.name}
+                        Signout
                       </Disclosure.Button>
-                    ))}
                   </div>
                 </div>
               </Disclosure.Panel>
